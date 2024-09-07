ab3f92683de0c6c7d3ad9fdf85a244a2ad98af5c
Himanshu-1-1 <pratapsinghh29@gmail.com>
2024-09-07 19:44:45 +0530

    Added dark mode feature in my portfolio

--- script.js
+++ script.js
@@ -4,3 +4,51 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+//darkmode
+const btn=document.getElementById("modetoggle");
+const btn2=document.getElementById("modetoggle2");
+const themeIcon = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+
+if (currentTheme==="dark"){
+    setDarkMode();
+}
+
+btn.addEventListener("click" , function (){
+    setTheme();
+});
+btn2.addEventListener("click" , function (){
+    setTheme();
+});
+
+function setTheme(){
+    let currentTheme=document.body.getAttribute("theme");
+    if(currentTheme==="dark"){
+        setlightMode();
+    }else{
+        setDarkMode();
+    }
+}
+
+function setDarkMode(){
+    document.body.setAttribute("theme","dark");
+    localStorage.setItem("theme","dark");
+
+    themeIcon.forEach((icon)=>{
+        icon.src = icon.getAttribute("src-dark")
+    })
+
+}
+
+
+function setlightMode(){
+    document.body.removeAttribute("theme");
+    localStorage.setItem("theme","light");
+
+    themeIcon.forEach((icon)=>{
+        icon.src = icon.getAttribute("src-light")
+    })
+
+}
